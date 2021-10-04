8cfa015086476c0ef062a55323f23fbbfb6ee607
mattjmason <masonmj10@gmail.com>
2021-10-03 22:25:18 -0400

    controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,12 @@
 class CategoriesController < ApplicationController
 
-    def index 
-        categories = Category.all 
-        render json: categories
+    def index
+        @categories = Category.all
+        render json: @categories, except: [:created_at, :updated_at], include: [:games]
     end
 
+    def show
+        @category = Category.find(params[:id])
+        render json: @category.games, except: [:created_at, :updated_at]
+    end
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,28 @@
 class GamesController < ApplicationController
 
     def index 
-        games = Game.all 
-        render json: games
+        @games = Game.all 
+        render json: @games, except: [:created_at, :updated_at], include: :category
     end
-#test comment
+
+    def create 
+        @game = Game.create(game_params)
+        if @game.save 
+        render json: @game, except: [:created_at, :updated_at]
+        else 
+            render json: {error: 'Could not be created'}
+        end
+    end
+
+    def destroy 
+        @game = Game.find(params[:id])
+        @game.destroy 
+        render json: {message: "Successfully deleted #{Game.name}!"}
+    end
+
+    private 
+
+    def game_params
+        params.require(:game).permit(:name, :genre, :published, :description, :category_id)
+    end
 end
