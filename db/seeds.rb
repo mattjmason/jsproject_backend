fe841ac3ee00a771804bf5ff889a2679692af3a1
mattjmason <masonmj10@gmail.com>
2021-10-05 22:44:09 -0400

    updated seed categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-first_person_shooter = Category.create(name: 'First Person Shooter')
 mmorpg = Category.create(name: 'MMORPG')
+first_person_shooter = Category.create(name: 'First Person Shooter')
+sci_fi = Category.create(name: 'Sci-Fi')
+third_person_shooter = Category.create(name: 'Third Person Shooter')
+cartoon = Category.create(name: 'Cartoon')
+
 
 destiny = Game.create(name: 'Destiny', genre: 'fantasy', published: '2012', description: 'power fantasy game', category: mmorpg)
 gears_of_war = Game.create(name: 'Gears of War', genre: 'sci-fi', published: '2011', description: 'save the world', category: first_person_shooter)
